d41a27643aa88ae13d58688468e51d42d7f7c037
buingoctai <buingoctai994@gmail.com>
2020-11-08 22:04:18 +0700

    refactor server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
+const { BUILD_MESSAGE } = require('./utils/constants');
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
@@ -21,36 +22,90 @@ io.on('connection', (socket) => {
         case 'mac':
             CLIENT['mac'] = socket.id;
             break;
-        case 'bot':
-            CLIENT['bot'] = socket.id;
+        case 'bot-win':
+            CLIENT['bot-win'] = socket.id;
+            break;
+        case 'bot-mac':
+            CLIENT['bot-mac'] = socket.id;
             break;
         default:
             break;
     }
-    console.log("----------update client------");
+
     socket.on('BUILD', (msg) => {
         const isWin = CLIENT.hasOwnProperty('win');
         const isMac = CLIENT.hasOwnProperty('mac');
-        const { buildPlatform, ...restMsg } = msg;
+        const isBotWin = CLIENT['bot-win'] === socket.id;
+        const isBotMac = CLIENT['bot-mac'] === socket.id;
 
-        if (buildPlatform === 'win' && isWin) {
+        if (isBotWin && isWin) {
             console.log("request build win");
-            io.to(CLIENT['win']).emit('REQUEST BUILDER', restMsg);
-        } else if (buildPlatform === 'mac' && isMac) {
+            io.to(CLIENT['win']).emit('REQUEST BUILDER', msg);
+        }
+        else if (isBotMac && isMac) {
             console.log("request build mac");
-            io.to(CLIENT['mac']).emit('REQUEST BUILDER', restMsg);
+            io.to(CLIENT['mac']).emit('REQUEST BUILDER', msg);
+        }
+        else if (isBotWin && !isWin) {
+            const msg = {
+                action: 'ERROR',
+                buildPlatform: 'win',
+                code: 'ERROR_WIN_NOT_READY',
+                message: BUILD_MESSAGE['ERROR_WIN_NOT_READY']
+            };
+            io.to(CLIENT['bot-win']).emit('OUTPUT BUILD', msg);
+        }
+        else if (isBotMac && !isMac) {
+            const msg = {
+                action: 'ERROR',
+                buildPlatform: 'mac',
+                code: 'ERROR_MAC_NOT_READY',
+                message: BUILD_MESSAGE['ERROR_MAC_NOT_READY']
+            };
+            io.to(CLIENT['bot-mac']).emit('OUTPUT BUILD', msg);
         }
         else {
-            io.to(CLIENT['bot']).emit('ERROR_INIT', 'ERROR_BUILDER_NOT_READY');
+            const msg = {
+                action: 'ERROR',
+                buildPlatform: (isWin && 'win') || (isMac && 'mac'),
+                code: 'ERROR_UNKNOW',
+                message: BUILD_MESSAGE['ERROR_UNKNOW']
+            };
+            io.to(CLIENT[socket.id]).emit('OUTPUT BUILD', msg);
         }
     });
 
     socket.on('OUTPUT BUILD', (output) => {
+        console.log('--------Output from builder----------');
         io.to(CLIENT['bot']).emit('OUTPUT BUILD', output);
+        switch (output.buildPlatform) {
+            case 'win':
+                io.to(CLIENT['bot-win']).emit('OUTPUT BUILD', output);
+                break;
+            case 'mac':
+                io.to(CLIENT['bot-mac']).emit('OUTPUT BUILD', output);
+                break;
+            default:
+                return;
+        }
+    });
+    socket.on('disconnect', () => {
+        console.log("--------Remove socket client----------");
+        console.log('disconnect', socket.id);
+        console.log('CLIENT', CLIENT);
+        for (const prop in CLIENT) {
+            if (CLIENT[prop] === socket.id) {
+                delete CLIENT[prop];
+            }
+        }
+        console.log('CLIENT', CLIENT);
     });
 });
 server.listen(port, () => {
-    console.log('listening on *:80');
+    console.log(`listening on *:${port}`);
+
 });
 
 // heroku logs --tail --app socket-build
+
+//547542490 / pass bananhhuynh!
--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,42 @@
 const PORT = 3000;
+const BUILD_MESSAGE = {
+    /* build state */
+    START_BUILD: 'Starting build',
+    CLONE_SOURCE: 'Cloning repository',
+    CHECKOUT_AND_BUILD: 'Checkout branch, build app',
+    /* new states*/
+    CHECKOUT_BRANCH: 'Checking out commit',
+    INSTALL_PACKAGES: 'Installing packages',
+    UPDATE_ABOUT_FILE: 'Updating ABOUT',
+    COMPILE_SASS_CREATE_LANG: 'Compiling SASS & translations',
+    BUILD_APP: 'Building electron app',
+    /* */
+    UPLOAD_FILE: 'Uploading file to remote server',
+    BUILD_SUCCESS: 'Build Success',
+    TIMEOUT: 'Websocket timed out.',
+    CLOSE_CONNECT: 'Websocket connection was closed by the client.',
+    /* notifications */
+    REGISTER_ID: 'Get build state successfully.',
+    CANCEL_SUCCESS: 'Build canceled successfully.',
+    CANCEL_FAILED: 'Unable to cancel build.',
+    GET_STATE_SUCCESS: 'Get build state successfully.',
+    GET_STATE_FAILED: 'Get build state failed.',
+    NOT_FOUND_ACTION: 'The requested action was not found.',
+    /* build  error */
+    ERROR_CONNECT: 'Websocket was errCode.',
+    ERROR_BUILDER: 'Builder was error in build processing.',
+    ERROR_BUILDER_UPLOAD: "The file was fail uploaded.",
+    ERROR_BUILDER_INVALID_BRANCH: 'The branch is invalid.',
+    ERROR_SERVER_CRASH: 'The connection was turned off by server.',
+    ERROR_UNKNOW: 'Unknow from server.',
+    /* socket server */
+    ERROR_WIN_NOT_READY: 'The win builder is not ready to serve now',
+    ERROR_MAC_NOT_READY: 'The mac builder is not ready to serve now',
+
+};
+
 
 module.exports = {
-    PORT
+    PORT,
+    BUILD_MESSAGE
 }
