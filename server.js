5bd71ee368e467fbc75681575924256be17e649d
buingoctai <buingoctai994@gmail.com>
2020-12-18 11:13:38 +0700

    add log

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
       query: { source },
     },
   } = socket;
-  console.log(id, source);
+  console.log('New connection init ', socket.id, source);
   switch (source) {
     case 'win':
       CLIENT['win'] = socket.id;
@@ -45,14 +45,14 @@ io.on('connection', (socket) => {
 
   socket.on('BUILD', (msg) => {
     console.log('###### Receive Request BUILD From Client ######');
-    console.log('CLIENT', CLIENT, 'socket.id',socket.id);
+    console.log('CLIENT', CLIENT, 'socket.id', socket.id);
     const isWin = CLIENT.hasOwnProperty('win');
     const isMac = CLIENT.hasOwnProperty('mac');
     const isBotWin = CLIENT['bot-win'] === socket.id;
     const isBotMac = CLIENT['bot-mac'] === socket.id;
     //const isBotWin = CLIENT['bot-win']?true:false;
     // const isBotMac = CLIENT['bot-mac']?true:false;
-   
+
     // save match group
     if (msg.action === 'STATE' && isBotWin) {
       matchGroupWin = msg.matchGroup;
